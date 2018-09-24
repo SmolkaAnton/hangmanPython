5d659170b603750d76ade5a546f95ce99acc61fb
Anton <Moose@Antons-MacBook-Pro.local>
2018-09-24 12:49:03 -0400

    Added number of attempts

--- hangman.py
+++ hangman.py
@@ -4,18 +4,36 @@ from random import randint
 
 #Main function
 def main():
-    words = ["hello", "world", "test"]
-    num = random()
-    word_generated = list(words[num]) #Chooses word from list user has to guess
-    playGame(word_generated)
+    animals = ["dog", "cat", "horse", "lion", "kangaroo", "parrot", "panda", "turle", "elephant", "tiger", "llama"]
+    adjectives = ["athletic", "happy", "sad", "fit", "dazzling", "magnificent", "mad", "lively", "petite", "brave", "thoughtful"]
+    states = ["alabama", "california", "delaware", "arizona", "nevada", "newyork", "northcarolina", "texas", "tennessee", "florida", "colorado"]
+    num = choose_word()
+    cat = choose_category()
+    if(cat==1):
+        category = "Animals"
+        word_generated = list(animals[num]) #Chooses word from list user has to guess
+        playGame(word_generated, category)
+    elif(cat==2):
+        category = "Adjectives"
+        word_generated = list(adjectives[num]) #Chooses word from list user has to guess
+        playGame(word_generated, category)
+    else:
+        category = "States"
+        word_generated = list(states[num]) #Chooses word from list user has to guess
+        playGame(word_generated, category)
 
-#function used to generate random number
-def random():
-    num = randint(0, 2)
+#function used to generate random number to choose word
+def choose_word():
+    num = randint(0, 10)
+    return num
+
+#Function used to generate number to choose category
+def choose_category():
+    num = randint(1,3)
     return num
 
 #Function used to play game
-def playGame(word_generated):
+def playGame(word_generated, category):
     display = []
     used = []
 
@@ -29,14 +47,16 @@ def playGame(word_generated):
 
     #Adds spaces between each _
     print(" ".join(display))
+    print("Category is: ", category)
     print()
 
     #Counter for guesses
     count = 0
-    while(count < len(word_generated)):
+    #Counter for incorrect guesses
+    incorrect = 5
+    while(count < len(word_generated) and incorrect >=0):
         guess = input("Guess a letter ")
         guess.lower()
-        print(count)
 
         #iterate through the generated word
         for i in range (len(word_generated)):
@@ -46,12 +66,17 @@ def playGame(word_generated):
                 used.remove(guess)
 
         if guess not in display:
-            print("Sorry, wrong guess")
+            print("Sorry, wrong guess. You have ", incorrect, " guesses left!")
+            incorrect -= 1
 
         print("You have guessed: ", count, " correct letters.")
+        print("Category is: ", category)
         print(" ".join(display))
         print()
 
-    print("You guessed the word!")
+    if(count == len(word_generated)):
+        print("You guessed the word!")
+    else:
+        print("Sorry, you didn't guess the word!")
 
 main()
